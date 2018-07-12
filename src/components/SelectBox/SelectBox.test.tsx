37a2ace29f3572149a10b04792d62b385af9ce40
Jay Kanive <jay.kanive@bigcommerce.com>
2018-07-12 13:45:47 -0700

    MERC-3698 Create unit tests for SelectBox

--- src/components/SelectBox/SelectBox.test.tsx
+++ src/components/SelectBox/SelectBox.test.tsx
@@ -1,15 +1,54 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 
+import { Select } from './styles';
 import SelectBox from './SelectBox';
 
-it('renders', () => {
-    const selectBox = shallow(
-        <SelectBox
-            label="Label"
-            options={[]}
-            name="test"
-        />
-    );
-    expect(selectBox).toMatchSnapshot();
+describe('<SelectBox />', () => {
+    it('renders', () => {
+        const selectBox = shallow(
+            <SelectBox
+                label="Label"
+                options={[]}
+                name="test"
+            />
+        );
+        expect(selectBox).toMatchSnapshot();
+    });
+
+    it('onChange is triggered and returns a value', () => {
+        const mockOnChange = jest.fn();
+        let event = {
+            target: { value: 'Google_Oswald_300' },
+        };
+        const options = [
+            {
+                label: 'Oswald',
+                value: 'Google_Oswald_400',
+            },
+            {
+                label: 'Oswald Light',
+                value: 'Google_Oswald_300',
+            },
+        ];
+        const selectBox = shallow(
+            <SelectBox
+                label="Label"
+                options={options}
+                name="logo-font"
+                onChange={mockOnChange}
+                selected="Google_Oswald_400"
+            />
+        );
+
+        selectBox.find(Select).simulate('change', event);
+        expect(mockOnChange).toBeCalledWith({'logo-font': 'Google_Oswald_300'});
+
+        event = {
+            target: { value: 'Google_Oswald_400' },
+        };
+
+        selectBox.find(Select).simulate('change', event);
+        expect(mockOnChange).toBeCalledWith({'logo-font': 'Google_Oswald_400'});
+    });
 });
