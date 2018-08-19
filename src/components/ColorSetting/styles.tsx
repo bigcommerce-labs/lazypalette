0d5d3126fb628333259c4778d6b8c2c958fb5dff
Erik Christensen <erik.christensen@bigcommerce.com>
2018-08-19 13:26:45 -0700

    MERC-3836 Update pattern lab to new select boxes, add hex color next to color settings

--- src/components/ColorSetting/styles.tsx
+++ src/components/ColorSetting/styles.tsx
@@ -11,15 +11,24 @@ interface SelectedColorProps {
     color: string;
 }
 export const SelectedColor = styled.div.attrs<SelectedColorProps>({}) `
-  background: ${(props: SelectedColorProps) => `${props.color}`};
-  border-radius: 3px;
-  height: 1.5rem;
-  width: 1.5rem;
-  cursor: pointer;
-  margin: 1px;
   align-self: center;
-  flex-shrink: 0;
+  background: ${(props: SelectedColorProps) => `${props.color}`};
   border: 1px solid ${({ theme }) => theme.colors.stroke};
+  border-radius: 3px;
+  box-sizing: border-box;
+  cursor: pointer;
+  flex-shrink: 0;
+  height: 1.5rem;
+  margin: 1px;
+  width: 1.5rem;
+`;
+
+export const ColorText = styled.div`
+    color: ${({ theme }) => theme.colors.secondaryText};
+    flex-grow: 1;
+    font-size: ${({ theme }) => theme.typography.fontSize.smaller};
+    margin-right: 0.5rem;
+    text-align: right;
 `;
 
 export const Label = styled.label`
