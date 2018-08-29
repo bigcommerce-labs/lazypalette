981d1f691061412f905689c7247e56639bdb57ae
Kevin Chu <kevin.chu@bigcommerce.com>
2018-08-29 11:46:18 -0700

    MERC-3901 Update store status banner copy and add shareable link

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 
-import { StyledBanner } from './styles';
+import { StyledBanner, StyledShareableLink } from './styles';
 
 interface BannerProps {
-    message: string;
+    isDownForMaintenance: boolean;
+    isPrelaunchStore: boolean;
 }
 
 const Banner = (props: BannerProps) => {
-    return (
-        <StyledBanner>
-            {props.message}
-        </StyledBanner>
-    );
+    const { isDownForMaintenance, isPrelaunchStore } = props;
+    const addBanner = isDownForMaintenance || isPrelaunchStore;
+    const message = isDownForMaintenance
+        ? 'Store is down for maintenance'
+        : isPrelaunchStore
+            ? 'Store has not launched and is not publicly visible'
+            : '';
+
+    if (!addBanner) {
+        return null;
+    } else {
+        return (
+            <StyledBanner>
+                {message}
+                {!props.isDownForMaintenance &&
+                    <StyledShareableLink>
+                        Copy private store link
+                    </StyledShareableLink>
+                }
+            </StyledBanner>
+        );
+    }
 };
 
 export default Banner;
