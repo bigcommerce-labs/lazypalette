223a8faa2b85f5e9d27d0c7d563313cf73e8d68e
Erik Christensen <erik.christensen@bigcommerce.com>
2018-09-08 19:14:47 -0700

    MERC-3980 Update undo/set active modal text, do not open modal on prelaunch save

--- src/components/PubShareBox/constants.tsx
+++ src/components/PubShareBox/constants.tsx
@@ -1,7 +1,28 @@
-export const enum Messages {
-    Publish = 'Publishing your store will make all changes publicly visible',
-    Reset = 'This theme has unpublished changes. Do you want to proceed?',
+export enum Modes {
+    ACTIVE = 'ACTIVE',
+    INACTIVE = 'INACTIVE',
+    PREVIEW = 'PREVIEW',
+    UNKNOWN = 'UNKNOWN',
 }
+
+export const PublishModalText = {
+    [Modes.ACTIVE]: {
+        action: 'Publish',
+        body: 'Publishing your store will make all changes publicly visible',
+        title: 'Publish Changes',
+    },
+    [Modes.INACTIVE]: {
+        action: 'Use As Active Theme',
+        body: 'You are about to make this the active theme in your store.',
+        title: 'Use As Active Theme',
+    },
+};
+
+export const ResetModalText = {
+    action: 'Undo Changes',
+    body: 'This will discard any changes made since the last time you saved.',
+    title: 'Undo Changes',
+};
 
 export const purchaseEndPoint = (variationId: string) =>
     `/manage/marketplace/themes/${variationId}?action=purchase`;
