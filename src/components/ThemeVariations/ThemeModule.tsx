44511cb4f9f76a2902dd0d04ff603b6dbf67202e
Erik Christensen <erik.christensen@bigcommerce.com>
2018-10-08 08:12:25 -0700

    MERC-4126 Selected and hover styling for Styles menu

--- src/components/ThemeVariations/ThemeModule.tsx
+++ src/components/ThemeVariations/ThemeModule.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { EntryActive, Item, List, Thumb, Title } from './styles';
+import { EntryActive, Item, List, Thumb, ThumbWrapper, Title } from './styles';
 import { ThemePropsList } from './ThemeVariations';
 
 const ThemeModule = (props: {variants: ThemePropsList, handleVariationChange(variationId: string): void}) => (
@@ -14,10 +14,9 @@ const ThemeModule = (props: {variants: ThemePropsList, handleVariationChange(var
                     {name}
                     {isActive && <EntryActive/>}
                 </Title>
-                <Thumb
-                    isActive={isActive}
-                    previewPath={image}
-                />
+                <ThumbWrapper isActive={isActive}>
+                    <Thumb previewPath={image}/>
+                </ThumbWrapper>
             </Item>
         ))}
     </List>
