45068a640d3521efc3c1ad544c852e79da30e125
Jay Kanive <jay.kanive@bigcommerce.com>
2018-10-12 11:38:07 -0700

    MERC-0000 Added redux-logger to non-prod env

--- src/middleware/middleware.ts
+++ src/middleware/middleware.ts
@@ -1,8 +1,17 @@
 import { applyMiddleware, compose } from 'redux';
+import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const middleware = [];
+
+if (process.env.NODE_ENV !== 'production') {
+    middleware.push(createLogger({ collapsed: true }));
+}
+
+middleware.push(thunk);
+
 export default composeEnhancers(
-    applyMiddleware(thunk)
+    applyMiddleware(...middleware)
 );
